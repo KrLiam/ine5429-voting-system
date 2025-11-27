64cc271661afd817fb0ec8ea4b48669adbf22a31
KrLiam <88288206+KrLiam@users.noreply.github.com>
2025-11-27 14:13:05 -0300

    adiciona persistencia ao servidor

--- server/main.py
+++ server/main.py
@@ -1,10 +1,14 @@
-from dataclasses import dataclass, field
+from pathlib import Path
 import secrets
 from typing import Any
 import phe
 import time
-from flask import Flask, request, send_from_directory, jsonify
+from random import SystemRandom
+from flask import Flask, json, request, send_from_directory, jsonify
 
+
+BULLETIN_BOARD_PATH = "data/bulletin_board.json"
+AUTHORITY_PATH = "data/authority.json"
 
 def generate_keys():
     return phe.generate_paillier_keypair(n_length=2048)
@@ -12,38 +16,120 @@ def generate_keys():
 
 Vote = list[phe.EncryptedNumber]
 
+def convert_vote(value: list[Any], public_key: phe.PaillierPublicKey) -> Vote:
+    result: list[phe.EncryptedNumber] = []
+    for v in value:
+        if isinstance(v, str):
+            v = int(v)
+        result.append(phe.EncryptedNumber(public_key, v))
+    return result
+
+
 class BulletinBoard:
+    start_time: int
+    duration: int
     tokens: dict[str, bool]
     votes: list[Vote]
 
-    def __init__(self, ntokens: int) -> None:
-        self.tokens = {}
-        self.votes = []
+    gen: SystemRandom
 
-        # generate tokens
+    def __init__(
+        self,
+        ntokens: int,
+        start_time: int,
+        duration: int,
+        tokens: dict[str, bool] | None = None,
+        votes: list[Vote] | None = None,
+
+    ) -> None:
+        self.start_time = start_time
+        self.duration = duration
+        self.tokens = tokens or {}
+        self.votes = votes or []
+
+        self.gen = SystemRandom()
+
+        if not self.tokens:
+            self.generate_tokens(ntokens)
+
+    @classmethod
+    def load_or_default(
+        cls,
+        path: str,
+        public_key: phe.PaillierPublicKey,
+        ntokens: int,
+        duration: int,
+    ) -> "BulletinBoard":
+        if Path(path).exists():
+            obj = json.load(open(path, "rt"))
+
+            start_time = obj.get("start_time", time.time())
+            duration = obj.get("duration", duration)
+
+            return BulletinBoard(
+                start_time=start_time,
+                duration=duration,
+                ntokens=len(obj.get("tokens", {})),
+                tokens=obj.get("tokens", {}),
+                votes=[convert_vote(v, public_key) for v in obj.get("votes", [])]
+            )
+        return BulletinBoard(ntokens, time.time(), duration)
+
+    @property
+    def end_time(self) -> float:
+        return self.start_time + self.duration
+    
+    def generate_tokens(self, ntokens: int):
         for _ in range(ntokens):
             self.tokens[secrets.token_hex(32)] = False
+        self.dump_data()
 
     def insert_vote(self, vote: Vote):
-        i = 0 # isso deve ser gerado aleatoriamente
+        i = self.gen.randrange(0, max(1, len(self.votes)))
         self.votes.insert(i, vote)
+        self.dump_data()
 
     def mark_token(self, token: str):
         if token not in self.tokens:
             return
         self.tokens[token] = True
+        self.dump_data()
     
     def dump_tokens(self, path: str):
         with open(path, "wt") as f:
             f.write("\n".join(key for key in self.tokens))
+    
+    def dump_data(self):
+        obj = {
+            "start_time": self.start_time,
+            "duration": self.duration,
+            "tokens": self.tokens,
+            "votes": [
+                [part.ciphertext() for part in vote]
+                for vote in self.votes
+            ],
+        }
+        json.dump(obj, open(BULLETIN_BOARD_PATH, "wt"), indent=4)
 
 
 class Authority:
     public_key: phe.PaillierPublicKey
     private_key: phe.PaillierPrivateKey
 
-    def __init__(self):
-        self.public_key, self.private_key = generate_keys()
+    def __init__(self, keys: tuple[phe.PaillierPublicKey, phe.PaillierPrivateKey] | None = None):
+        keys = keys or generate_keys()
+        self.public_key, self.private_key = keys
+    
+    @classmethod
+    def load_or_default(self, path: str) -> "Authority":
+        if Path(path).exists():
+            obj = json.load(open(path, "rt"))
+
+            public = phe.PaillierPublicKey(obj["n"])
+            private = phe.PaillierPrivateKey(public, obj["p"], obj["q"])
+            return Authority((public, private))
+        
+        return Authority()
 
     def get_public_key(self) -> phe.PaillierPublicKey:
         return self.public_key
@@ -51,13 +137,22 @@ class Authority:
     def reveal(self, result: list[phe.EncryptedNumber]) -> list[int]:
         return [self.private_key.decrypt(v) for v in result]
 
+    def save(self, path: str):
+        json.dump(
+            {
+                "n": self.public_key.n,
+                "p": self.private_key.p,
+                "q": self.private_key.q,
+            },
+            open(path, "wt"),
+            indent=4
+        )
+
 
 class Server:
     authority: Authority
     public_key: phe.PaillierPublicKey
     about: str
-    start_time: float
-    duration: float
     candidates: list[str]
     tokens: dict[str, bool]
 
@@ -74,12 +169,16 @@ class Server:
         ):
         self.authority = authority
         self.public_key = authority.get_public_key()
-        self.start_time = time.time()
-        self.duration = 60 * duration # 1 hora
         self.candidates = candidates
         self.about = about
 
-        self.bb = BulletinBoard(ntokens)
+        duration = 60 * duration # 1 hora
+        self.bb = BulletinBoard.load_or_default(
+            BULLETIN_BOARD_PATH,
+            self.public_key,
+            ntokens,
+            duration,
+        )
 
         if token_dump_path is not None:
             self.bb.dump_tokens(token_dump_path)
@@ -91,25 +190,13 @@ class Server:
         )
 
         self.register_routes()
-    
-    @property
-    def end_time(self) -> float:
-        return self.start_time + self.duration
 
     def over(self) -> bool:
-        return time.time() >= self.end_time
+        return time.time() >= self.bb.end_time
     
     def validate_token(self, token: str) -> bool:        
         return token in self.bb.tokens and not self.bb.tokens[token]
     
-    def convert_vote(self, value: list[Any]) -> list[phe.EncryptedNumber]:
-        result: list[phe.EncryptedNumber] = []
-        for v in value:
-            if isinstance(v, str):
-                v = int(v)
-            result.append(phe.EncryptedNumber(self.public_key, v))
-        return result
-
     def register_routes(self):
         @self.app.route("/")
         def serve_index():
@@ -120,7 +207,7 @@ class Server:
         def get_election():
             return jsonify({
                 "key": {"n": str(self.public_key.n), "g": str(self.public_key.g)},
-                "end_time": self.end_time,
+                "end_time": self.bb.end_time,
                 "about": self.about,
                 "candidates": self.candidates,
             })
@@ -148,7 +235,7 @@ class Server:
                     "message": "Token inválido."
                 })
 
-            vote = self.convert_vote(values)
+            vote = convert_vote(values, self.public_key)
 
             self.bb.mark_token(token)
             self.bb.insert_vote(vote)
@@ -186,7 +273,8 @@ class Server:
 
 
 if __name__ == "__main__":
-    authority = Authority()
+    authority = Authority.load_or_default(AUTHORITY_PATH)
+    authority.save(AUTHORITY_PATH)
 
     server = Server(
         authority=authority,
